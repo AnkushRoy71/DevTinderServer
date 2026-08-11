2db56052f1b28a6e3b503c462efd325fce37b288
AnkushRoy71 <royankush832@gmail.com>
2026-08-11 22:23:01 +0530

    created 2 new API for getting single and all user

--- src/app.js
+++ src/app.js
@@ -3,14 +3,13 @@ const dotenv = require("dotenv").config();
 const connectDB = require("./config/database");
 const userModel = require("./models/user");
 
-
 const app = express();
 
 app.use(express.json());
 
-app.post("/signup", async(req,res)=>{
+app.post("/signup", async (req, res) => {
   const user = req.body;
-  if(!user) return res.status(400).send("User data is required");
+  if (!user) return res.status(400).send("User data is required");
   const newUser = new userModel(user);
   try {
     await newUser.save();
@@ -19,17 +18,41 @@ app.post("/signup", async(req,res)=>{
     console.error(err);
     res.status(500).send("Error creating user");
   }
-})
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const emailId = req.body.email;
+    if (!emailId) return res.status(400).send("Email is required");
+    const users = await userModel.find({ email: emailId });
+    if (users.length === 0) return res.status(404).send("User not found");
+    res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching users");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const feed = await userModel.find();
+    if (feed.length === 0) return res.status(404).send("No feed available");
+    res.status(200).json(feed);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching feed");
+  }
+});
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-connectDB().then(() =>{
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
-})
-.catch((err) => {
-  console.error("Failed to connect to the database", err);
-})
-
+connectDB()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database", err);
+  });
